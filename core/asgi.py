c4e5a7d737077ba51e3eb00ccdc767470ea59c65
javier sepulveda <j.sepulveda.ib@gmail.com>
2026-06-05 14:21:37 -0400

    .

--- core/asgi.py
+++ core/asgi.py
@@ -1,13 +1,18 @@
 import os
 from django.core.asgi import get_asgi_application
+
+# Configurar DJANGO_SETTINGS_MODULE antes de cualquier otra importación de Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Inicializar la aplicación ASGI de Django primero para cargar los modelos y settings
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import core.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             core.routing.websocket_urlpatterns
