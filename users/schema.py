3cb3b2f7438003838af555d8c2276f701e90345b
javier sepulveda <j.sepulveda.ib@gmail.com>
2026-08-06 02:23:31 -0400

    .

--- users/schema.py
+++ users/schema.py
@@ -213,11 +213,10 @@ class ProfessionalProfileType(DjangoObjectType):
         completed_jobs_count = Job.objects.filter(
             professional=self.user,
             status='FINISHED',
-            # updated_at__gte=self.plan_start_date
+            updated_at__gte=self.plan_start_date
         ).count()
         
-        # DEBUG TESTING
-        return True
+        return completed_jobs_count >= self.plan.max_completed_jobs
 
 class Query(graphene.ObjectType):
     me = graphene.Field(UserType)
